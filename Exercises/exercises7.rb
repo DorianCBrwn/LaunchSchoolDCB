f8b7bc3b57a00cce759cdf7c059ea4ea3ea5ed96
Dorian Brown <brown.3794@gmail.com>
2018-12-24 00:10:50 -0500

    add exercies folder and files 17-

---
+++ Exercises/exercises7.rb
@@ -0,0 +1,2 @@
+#The major difference between an Array and a Hash is the array is ordered and indexed whereas in a hash values are
+#stored in key value pairs.
---
+++ Exercises/exercises8.rb
@@ -0,0 +1,2 @@
+hash1 = {"key" => 1}
+hash2 = {key: 1}
