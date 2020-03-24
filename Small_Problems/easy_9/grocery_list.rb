75a02f4cca5047526624cfdba2f0965c7f5ef262
Dorian Brown <brown.3794@gmail.com>
2020-03-24 10:10:48 -0400

    create Lesson 6 directory

--- Small_Problems/easy_9/grocery_list.rb
+++ Small_Problems/easy_9/grocery_list.rb
@@ -1,6 +1,40 @@
-Write a method which takes a grocery list (array) of fruits with quantities and converts it into an array of the correct number of each fruit.
+#Grocery List
+=begin 
+Problem:
+Write a method which takes a grocery list (array) of fruits with quantities and converts it into an array of the correct
+number of each fruit.
+Examples/tests: 
+buy_fruit([["apples", 3], ["orange", 1], ["bananas", 2]]) ==
+  ["apples", "apples", "apples", "orange", "bananas","bananas"]
 
-Example:
+INPUT: nested array 3 element array
+      - each inner array contains 
+          1 word string in first index
+          int in second index
+
+OUTPUT: array of strings
+Rules:
+    Explicit: 
+    Implicit: order of inner words does not change
+
+D: Array
+
+Approach: 
+Define method buy_fruit with parameter groceries
+define variable result
+Iterate through array groceries
+  for each inner item 
+  push item[0] to result item[1] times
+return result
+C:
+=end
+def buy_fruit(groceries)
+  result = []
+  groceries.each do |item|
+    item.last.times { result << item.first }
+  end
+  result
+end
 
 buy_fruit([["apples", 3], ["orange", 1], ["bananas", 2]]) ==
   ["apples", "apples", "apples", "orange", "bananas","bananas"]
