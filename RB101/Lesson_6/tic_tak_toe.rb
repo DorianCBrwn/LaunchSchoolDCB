14a5cd7fede2ce800431ca3a5a81e8f64afe6760
Dorian Brown <brown.3794@gmail.com>
2020-03-26 12:57:10 -0400

    Add determining the winner logic

--- RB101/Lesson_6/tic_tak_toe.rb
+++ RB101/Lesson_6/tic_tak_toe.rb
@@ -1,3 +1,4 @@
+require 'pry'
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -7,7 +8,7 @@ puts  "=> #{msg}"
 end
 
 def display_board(brd)
-  system clear
+  system('clear')
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}  "
@@ -23,7 +24,7 @@ def display_board(brd)
   puts ""
 end
 
-def initialize_board 
+def initialize_board
   new_board = {}
   (1..9).each {|num| new_board[num] = INITIAL_MARKER }
   new_board
@@ -35,7 +36,7 @@ end
 
  def player_places_piece!(brd)
   square = ''
-  loop do 
+  loop do
     prompt "Choose a square #{empty_squares(brd).join(', ')}"
     square = gets.chomp.to_i
   break if empty_squares(brd).include?(square)
@@ -54,15 +55,44 @@ end
  end
 
  def someone_won?(brd)
-   false
+   !!detect_winner(brd)
+ end
+
+ def detect_winner(brd)
+   winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                   [[1, 5, 9], [3, 5, 7]]              # diagonals
+
+   winning_lines.each do |line| 
+     if brd[line[0]] == PLAYER_MARKER &&
+        brd[line[1]] == PLAYER_MARKER &&
+        brd[line[2]] == PLAYER_MARKER
+       return 'Player'
+     elsif brd[line[0]] == COMPUTER_MARKER &&
+           brd[line[1]] == COMPUTER_MARKER &&
+           brd[line[2]] == COMPUTER_MARKER
+       return 'Computer'
+     end
+   end
+   nil
  end
 
  board = initialize_board
- display_board(board)
 
-loop do 
-  player_places_piece!(board)
-  computer_places_piece!(board)
+loop do
   display_board(board)
+
+  player_places_piece!(board)
   break if someone_won?(board) || board_full?(board)
+
+  computer_places_piece!(board)
+  break if someone_won?(board) || board_full?(board)
+end
+
+display_board(board)
+
+if someone_won?(board)
+  prompt "#{detect_winner(board)} won!"
+else
+  prompt "It's a tie!"
 end
