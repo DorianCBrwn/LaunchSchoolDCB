b974225ce1fec006e798225b2cbeabe3aa22b9bb
Dorian Brown <brown.3794@gmail.com>
2020-04-07 12:40:19 -0400

    Add attack and defense logic for computer player

--- RB101/Lesson_6/tic_tak_toe.rb
+++ RB101/Lesson_6/tic_tak_toe.rb
@@ -6,14 +6,54 @@ WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
+CHOOSE = "choose"
+PLAYER = 'player'
+COMPUTER = 'computer'
+
+def choose_play_order
+  loop do 
+    prompt "pick who goes first: enter 'p' for player or 'c' for computer"
+    choice = gets.chomp
+    if choice.downcase.start_with?('p')
+      prompt "Great the #{PLAYER} will go first."
+      return choice = PLAYER
+    elsif choice.downcase.start_with?('c')
+      prompt "Great the #{COMPUTER} will go first."
+      return choice = COMPUTER
+    else 
+      prompt "invalid response, please choose who will go first"
+      return choice = CHOOSE
+    end
+    break unless choice == CHOOSE
+  end
+end
+
+def set_play_order(choice)
+  
+end
+
+
+def find_at_risk_square(brd, mark)
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(mark) == 2
+      x = line.reject{ |idx| brd[idx] == mark}.pop
+    return x if empty_squares(brd).include?(x)
+     else 
+       next
+      end
+    end
+  nil
+end
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+scoreboard = { player_w: 0, computer_w: 0 }
+
 # rubocop: disable Metrics/AbcSize
 def display_board(brd)
-  system('clear')
+  # system('clear')
   puts "You're a #{PLAYER_MARKER}. Computer is a #{COMPUTER_MARKER}"
   puts ""
   puts "     |     |"
@@ -37,11 +77,14 @@ def initialize_board
   new_board
 end
 
-def joiner(arr)
-  result = ""
-  arr.each do |number|
-    result << number.to_s + ',' unless arr.last? == number
-    result <<  ' or ' + number.to_s
+def joiner(arr, separator= ',', word= 'or')
+  case arr.size
+  when 0 then ''
+  when 1 then arr.first
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}"
+    arr.join(separator)
   end
 end
 
@@ -63,7 +106,17 @@ def player_places_piece!(brd)
 end
 
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
+  #attack
+  square = find_at_risk_square(brd, COMPUTER_MARKER)
+  #defend
+  if square.nil?
+  square = find_at_risk_square(brd, PLAYER_MARKER) if find_at_risk_square(brd, PLAYER_MARKER) 
+  end
+
+  if square.nil? 
+     square = empty_squares(brd).sample
+  end
+
   brd[square] = COMPUTER_MARKER
 end
 
@@ -80,16 +133,39 @@ def detect_winner(brd)
     if brd.values_at(*lines).count(PLAYER_MARKER) == 3
       return 'Player'
     elsif brd.values_at(*lines).count(COMPUTER_MARKER) == 3
-      return 'Player'
+      return 'Computer'
     end
   end
   nil
+end
+
+def keep_score(scoreboard)
+  restart = false
+  if scoreboard[:player_w] >= 5
+    prompt "Congradulations, you won the set"
+    restart = true
+  elsif scoreboard[:computer_w] >= 5
+    prompt "Oh no, you lost the set"
+    restart = true
+  else
+    prompt "The current score: Player: #{scoreboard[:player_w]} - Computer: #{scoreboard[:computer_w] } wins"
+  end
+  restart
+end
+
+def track_winners(brd, scoreboard)
+  winner = detect_winner(brd)
+   case winner
+   when 'Player' then scoreboard[:player_w] += 1
+   when 'Computer' then scoreboard[:computer_w] += 1
+   end
 end
 
 loop do
   board = initialize_board
 
   loop do
+    choose_play_order
     display_board(board)
 
     player_places_piece!(board)
@@ -106,6 +182,11 @@ loop do
   else
     prompt "It's a tie!"
   end
+
+  track_winners(board, scoreboard)
+
+
+ break if keep_score(scoreboard)
 
   prompt "Play again? ( y or n )"
   answer = gets.chomp
