f2ed7579272cc1f3d090379a4d02d4f30829870f
Dorian Brown <brown.3794@gmail.com>
2021-03-16 17:38:30 -0400

    Finish TodoList Assignment

---
+++ RB130/Lesson1/TodoList_complete.rb
@@ -0,0 +1,207 @@
+# This class represents a todo item and its associated
+# data: name and description. There's also a "done"
+# flag to show whether this todo item is done.
+
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+
+  def ==(otherTodo)
+    title == otherTodo.title &&
+      description == otherTodo.description &&
+      done == otherTodo.done
+  end
+end
+
+# This class represents a collection of Todo objects.
+# You can perform typical collection-oriented actions
+# on a TodoList object, including iteration and selection.
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  def add(todo_item)
+    begin
+      if validate_todo_type(todo_item)
+        todos << todo_item
+      else
+        raise TypeError.new("Can only add Todo objects")
+      end
+    rescue TypeError => e
+      puts e.message
+    end
+  end
+
+  alias_method  :<<, :add
+
+  def size
+    todos.size
+  end
+
+  def first
+    todos.first
+  end
+
+  def last
+    todos.last
+  end
+
+  def to_a
+    todos.clone
+  end
+
+  def done?
+    todos.all?(&:done?)
+  end
+
+  def item_at(index)
+    todos.fetch(index)
+  end
+
+  def mark_done_at(index)
+    item_at(index).done!
+    puts "Todo: #{item_at(index).title} is done!"
+  end
+
+  def mark_undone_at(index)
+    item_at(index).undone!
+    puts "Todo: #{item_at(index).title} not done!"
+  end
+
+  def done!
+    todos.map(&:done!)
+  end
+
+  def shift
+    todos.shift
+  end
+
+  def pop
+    todos.pop
+  end
+
+  def remove_at(index)
+    item_at(index)
+    todos.delete_at(index)
+  end
+
+  def to_s
+    text = "---- #{title} ----\n"
+    text << todos.map(&:to_s).join("\n")
+    text
+  end
+
+  def each
+    counter = 0
+    while counter < todos.size
+      yield(todos[counter])
+      counter += 1
+    end
+    self
+  end
+
+  def select
+    results = TodoList.new(title)
+    each do |todo|
+      results << todo if yield(todo)
+    end
+    results
+  end
+
+  def find_by_title(title)
+    each do |todo|
+      return todo if todo.title == title
+    end
+    nil
+  end
+
+  def all_done
+    select do |todo|
+      todo.done?
+    end
+  end
+
+  def all_not_done
+    select do |todo|
+      todo.done? == false
+    end
+  end
+
+  def mark_done(string)
+    each do |todo|
+      todo.done! if todo.title == string
+    end
+  end
+
+  def mark_all_done
+    each do |todo|
+      todo.done!
+    end
+  end
+
+  def mark_all_undone
+    each do |todo|
+      todo.undone!
+    end
+  end
+
+  private
+  attr_accessor :todos
+
+  def validate_todo_type(todo_item)
+    todo_item.class == Todo
+  end
+
+
+end
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+
+list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+
+todo1.done!
+
+p list.find_by_title("Buy milk")
+p list.find_by_title("This is not a Todo")
+
+p list.all_done
+p list.all_not_done
+ list.mark_done("Clean room")
+ list.mark_all_done
+p list.all_done
+p list.mark_all_undone
+p list.all_not_done
