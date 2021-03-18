ea11d9bc9089086c55b93d992370e2540732d6e5
Dorian Brown <brown.3794@gmail.com>
2021-03-18 17:40:25 -0400

    add assert equality test

--- RB130/Lesson2/car_test.rb
+++ RB130/Lesson2/car_test.rb
@@ -1,44 +1,16 @@
 require 'minitest/autorun'
-require ''
 
 require_relative 'car'
 
 class CarTest < MiniTest::Test
-  def setup
-    @car = Car.new
-  end
+  def test_value_equality
+    car1 = Car.new
+    car2 = Car.new
 
-  def test_car_exists
-    car = Car.new
-    assert(car)
-  end
+    car1.name = "Kim"
+    car2.name = "Kim"
 
-  def test_wheels
-    car = Car.new
-    assert_equal(4, car.wheels)
-  end
-
-  def test_name_is_nil
-    car = Car.new
-    assert_nil(car.name)
-  end
-
-  def test_raise_initialize_with_arg
-    assert_raises(ArgumentError) do
-      car = Car.new(name: "Joey")
-    end
-  end
-
-  def test_instance_of_car
-    car = Car.new
-    assert_instance_of(Car, car)
-  end
-
-  def test_includes_car
-    car = Car.new
-    arr = [1, 2, 3]
-    arr << car
-
-    assert_includes(arr, car)
+    assert_equal(car1, car2)          # this will pass
+    assert_same(car1, car2)           # this will fail
   end
 end
