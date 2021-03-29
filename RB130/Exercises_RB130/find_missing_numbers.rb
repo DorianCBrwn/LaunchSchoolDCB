52c2e10d06e515886e5d9e28fbcf36c42a2de7b8
Dorian Brown <brown.3794@gmail.com>
2021-03-29 15:50:16 -0400

    Add Ls Solution to find_missing_numbers.rb

---
+++ RB130/Exercises_RB130/find_missing_numbers.rb
@@ -0,0 +1,18 @@
+def missing(arr)
+  arr_range = (arr[0]..arr[-1]).to_a
+  arr_range - arr
+end
+
+p missing([-3, -2, 1, 5]) == [-1, 0, 2, 3, 4]
+p missing([1, 2, 3, 4]) == []
+p missing([1, 5]) == [2, 3, 4]
+p missing([6]) == []
+
+#LS Solution
+def missing(array)
+  result = []
+  array.each_cons(2) do |first, second|
+    result.concat(((first + 1)..(second - 1)).to_a)
+  end
+  result
+end
