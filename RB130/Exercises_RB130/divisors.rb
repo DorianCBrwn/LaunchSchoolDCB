0894850c4df280240b793643f7149494c9e7ea22
Dorian Brown <brown.3794@gmail.com>
2021-04-05 12:16:37 -0400

    Finish exercise divisors

--- RB130/Exercises_RB130/divisors.rb
+++ RB130/Exercises_RB130/divisors.rb
@@ -1,9 +1,41 @@
-
-
-
-
+=begin
+Problem: Write a method that returns a list of all of the divisors of the positive integer passed in as an argument. The return value can be in any sequence you wish.
+Rephrase: list all the divisors of a given int
+Examples/tests:
 divisors(1) == [1]
 divisors(7) == [1, 7]
 divisors(12) == [1, 2, 3, 4, 6, 12]
 divisors(98) == [1, 2, 7, 14, 49, 98]
 divisors(99400891) == [1, 9967, 9973, 99400891] # may take a minute
+INPUT: Integer
+OUTPUT: Array of ints
+Rules:
+  Explicit:
+  Implicit: A divisors is a number that divides into another without a remainder.
+D: Array
+Approach:
+Define method divisors with parameter int
+create empty array divisors
+Iterate from 1 through int
+Check if int is divisible by each number
+if divisible push number to divisors
+else
+  next
+return divisors array
+end
+C:
+=end
+
+def divisors(int)
+  (1..int).select do |i|
+    (int % i) == 0
+  end
+end
+
+
+
+p divisors(1) == [1]
+p divisors(7) == [1, 7]
+p divisors(12) == [1, 2, 3, 4, 6, 12]
+p divisors(98) == [1, 2, 7, 14, 49, 98]
+p divisors(99400891) == [1, 9967, 9973, 99400891] # may take a minute
