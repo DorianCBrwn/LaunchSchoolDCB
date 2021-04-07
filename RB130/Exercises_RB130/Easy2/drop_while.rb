6113fa2221b83b34a29607d2f5817d90f2065818
Dorian Brown <brown.3794@gmail.com>
2021-04-07 14:57:01 -0400

    Complete exercise drop_while.rb

---
+++ RB130/Exercises_RB130/Easy2/drop_while.rb
@@ -0,0 +1,15 @@
+def drop_while(col)
+start_point = 0
+  while start_point < col.size
+    break unless yield(col[start_point])
+    start_point += 1
+  end
+  col.slice(start_point, col.length)
+end
+
+p drop_while([1, 3, 5, 6]) { |value| value.odd? } == [6]
+p drop_while([1, 3, 5, 6]) { |value| value.even? } == [1, 3, 5, 6]
+p drop_while([1, 3, 5, 6]) { |value| true } == []
+p drop_while([1, 3, 5, 6]) { |value| false } == [1, 3, 5, 6]
+p drop_while([1, 3, 5, 6]) { |value| value < 5 } == [5, 6]
+p drop_while([]) { |value| true } == []
