d0f904bea1db5c0b4b7aaf13562c57805869b587
Dorian Brown <brown.3794@gmail.com>
2021-04-08 11:49:22 -0400

    complete each cons 2

--- RB130/Exercises_RB130/Easy2/each_cons_2.rb
+++ RB130/Exercises_RB130/Easy2/each_cons_2.rb
@@ -1,34 +1,37 @@
-def each_cons(col)
-  return col.to_enum unless block_given?
-  (0...col.size - 1 ).each_with_object(nil) { |i| yield(col.slice(i, 2)) }
+def each_cons(arr, n)
+  return arr.to_enum unless block_given?
+  arr.each_with_object(nil).with_index do |(ele,_), idx|
+    break if idx + n > arr.size
+    n > 1 ? yield(arr[idx, n]) :yield(ele)
+  end
 end
 
 hash = {}
 each_cons([1, 3, 6, 10], 1) do |value|
   hash[value] = true
 end
-hash == { 1 => true, 3 => true, 6 => true, 10 => true }
+p hash == { 1 => true, 3 => true, 6 => true, 10 => true }
 
 hash = {}
 each_cons([1, 3, 6, 10], 2) do |value1, value2|
   hash[value1] = value2
 end
-hash == { 1 => 3, 3 => 6, 6 => 10 }
+p hash == { 1 => 3, 3 => 6, 6 => 10 }
 
 hash = {}
 each_cons([1, 3, 6, 10], 3) do |value1, *values|
   hash[value1] = values
 end
-hash == { 1 => [3, 6], 3 => [6, 10] }
+p hash == { 1 => [3, 6], 3 => [6, 10] }
 
 hash = {}
 each_cons([1, 3, 6, 10], 4) do |value1, *values|
   hash[value1] = values
 end
-hash == { 1 => [3, 6, 10] }
+p hash == { 1 => [3, 6, 10] }
 
 hash = {}
 each_cons([1, 3, 6, 10], 5) do |value1, *values|
   hash[value1] = values
 end
-hash == {}
+p hash == {}
