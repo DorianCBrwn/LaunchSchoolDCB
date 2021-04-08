3935c51fcc9d4ff3ef0e6eefc493fcf990ed1aea
Dorian Brown <brown.3794@gmail.com>
2021-04-08 10:38:09 -0400

    complete each_cons.rb part 1

---
+++ RB130/Exercises_RB130/Easy2/each_cons.rb
@@ -0,0 +1,26 @@
+def each_cons(col)
+  return col.to_enum unless block_given?
+  (0...col.size - 1 ).each_with_object(nil) { |i| yield(col.slice(i, 2)) }
+end
+
+hash = {}
+result = each_cons([1, 3, 6, 10]) do |value1, value2|
+  hash[value1] = value2
+end
+p result == nil
+p hash == { 1 => 3, 3 => 6, 6 => 10 }
+
+hash = {}
+result = each_cons([]) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == {}
+p result == nil
+
+hash = {}
+result = each_cons(['a', 'b']) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == {'a' => 'b'}
+p result == nil
+p each_cons([1, 3, 6, 10])
