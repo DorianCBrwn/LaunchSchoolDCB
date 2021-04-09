a8ff0c7ce27d2fb4817373b79edd13e16989d8f5
Dorian Brown <brown.3794@gmail.com>
2021-04-09 15:36:20 -0400

    finish passing parameters3

--- RB130/Exercises_RB130/Medium_1/passing_parameter3.rb
+++ RB130/Exercises_RB130/Medium_1/passing_parameter3.rb
@@ -5,3 +5,27 @@ def gather(items)
   yield(items)
   puts "We've finished gathering!"
 end
+
+puts "Example 1"
+gather(items) do |*head,tail|
+  puts head.join(", ")
+  puts tail
+end
+
+puts "Example 2"
+gather(items) do |start, *middle, last|
+  puts start
+  puts middle.join(", ")
+  puts last
+end
+
+puts "Example 3"
+gather(items) do |head, *rest|
+  puts head
+  puts rest.join(", ")
+end
+
+puts "Example 4"
+gather(items) do | a, b, c, d|
+  puts [a,b,c].join(", ") + ", and #{d}"
+end
