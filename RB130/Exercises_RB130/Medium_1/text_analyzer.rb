5acb8e0c2d28236900c62740436ce17469b233de
Dorian Brown <brown.3794@gmail.com>
2021-04-12 13:32:20 -0400

    Finish medium 1 exercises

--- RB130/Exercises_RB130/Medium_1/text_analyzer.rb
+++ RB130/Exercises_RB130/Medium_1/text_analyzer.rb
@@ -7,4 +7,6 @@ class TextAnalyzer
 end
 
 analyzer = TextAnalyzer.new
-analyzer.process{ |lines| p lines}
+analyzer.process{ |lines| puts lines.scan(/\w\b/).size.to_s + " words"}
+analyzer.process{ |lines| puts lines.scan(/^[A-Z]\w+ /).size.to_s + " paragraphs"}
+analyzer.process{ |lines| puts ( 1 + lines.scan(/\n/).size).to_s + " lines"}
