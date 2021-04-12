5bf1df4f4c2ce6fb13884a40fd27734f0fdaa0df
Dorian Brown <brown.3794@gmail.com>
2021-04-12 17:02:43 -0400

    Complete test, accept money

--- RB130/Exercises_RB130/Medium_2_Testing/cash_register_test.rb
+++ RB130/Exercises_RB130/Medium_2_Testing/cash_register_test.rb
@@ -7,4 +7,13 @@ require_relative 'transaction'
 
 class CashRegisterTest < MiniTest::Test
 
+ def setup
+  @cash_register1 = CashRegister.new(100)
+  @purchase1 = Transaction.new(25.00)
+  @purchase1.amount_paid = 25.00
+ end
+
+ def test_accept_money
+  assert_equal 125, @cash_register1.accept_money(@purchase1)
+ end
 end
