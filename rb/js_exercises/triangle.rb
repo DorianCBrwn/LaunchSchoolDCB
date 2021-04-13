d47d108a378f426b4c457b6f2e27b15265638ba0
Dorian Brown <brown.3794@gmail.com>
2021-04-13 15:39:42 -0400

    feat(triangle) start basic outline for triangle.rb

---
+++ rb/js_exercises/triangle.rb
@@ -0,0 +1,27 @@
+=begin
+Problem:
+Rephrase:
+Examples/tests:
+INPUT:
+OUTPUT:
+Rules:
+  Explicit:
+        - To be a valid triangle, each side length must be greater than 0.
+        - To be a valid triangle, the sum of the lengths of any two sides must be greater than or equal to the length of the remaining side.
+
+        - An equilateral triangle has three sides of equal length.
+        - An isosceles triangle has exactly two sides of the equal length.
+        - A scalene triangle has three sides of unequal length (no two sides have equal length).
+
+
+  Implicit:
+D:
+Approach:
+C:
+=end
+class Triangle
+  def initialize(s1,s2,s3)
+
+  end
+
+end
