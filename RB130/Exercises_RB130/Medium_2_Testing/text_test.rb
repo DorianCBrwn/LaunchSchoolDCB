4bd023bd3c8dbc0cd89f754b7e61dd8ff933411a
Dorian Brown <brown.3794@gmail.com>
2021-04-13 11:23:56 -0400

    complete test count

--- RB130/Exercises_RB130/Medium_2_Testing/text_test.rb
+++ RB130/Exercises_RB130/Medium_2_Testing/text_test.rb
@@ -6,17 +6,22 @@ require_relative 'text'
 class TextTest < Minitest::Test
   def setup
     @sample = File.open("text.txt")
-    @text = Text.new(@sample.read)
   end
 
   def teardown
     @sample.close
+    puts "File has been closed: #{@sample.closed?}"
   end
 
   def test_swap
     expected = File.open("text.txt"){|f| f.read}
-    assert_equal expected.gsub!('a', 'e'), @text.swap('a', 'e')
+    text = Text.new(@sample.read)
+    assert_equal expected.gsub!('a', 'e'), text.swap('a', 'e')
   end
 
+  def test_count
+    text = Text.new(@sample.read)
+    assert_equal 72, text.word_count
+  end
 
 end
