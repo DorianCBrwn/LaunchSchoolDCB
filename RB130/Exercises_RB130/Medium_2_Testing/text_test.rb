49cbc9d2474c62ec5d8323e61757b18bc9c83d24
Dorian Brown <brown.3794@gmail.com>
2021-04-13 10:53:27 -0400

    complete test swap

--- RB130/Exercises_RB130/Medium_2_Testing/text_test.rb
+++ RB130/Exercises_RB130/Medium_2_Testing/text_test.rb
@@ -1,9 +1,22 @@
 require 'minitest/autorun'
-require 'minitest/reporters'
+require "minitest/reporters"
 Minitest::Reporters.use!
 
 require_relative 'text'
+class TextTest < Minitest::Test
+  def setup
+    @sample = File.open("text.txt")
+    @text = Text.new(@sample.read)
+  end
 
-class Text < Minitest::Test
+  def teardown
+    @sample.close
+  end
+
+  def test_swap
+    expected = File.open("text.txt"){|f| f.read}
+    assert_equal expected.gsub!('a', 'e'), @text.swap('a', 'e')
+  end
+
 
 end
