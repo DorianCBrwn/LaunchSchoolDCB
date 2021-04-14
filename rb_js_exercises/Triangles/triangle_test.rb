0b569ee91bf6981981edb14ee1d9b2c4302de3f0
Dorian Brown <brown.3794@gmail.com>
2021-04-14 13:36:26 -0400

    test(triangle_test) assert all tests

--- rb_js_exercises/Triangles/triangle_test.rb
+++ rb_js_exercises/Triangles/triangle_test.rb
@@ -67,28 +67,24 @@ class TriangleTest < Minitest::Test
   end
 
   def test_size_inequality_is_illegal
-    skip
     assert_raises(ArgumentError) do
       triangle = Triangle.new(1, 1, 3)
     end
   end
 
   def test_size_inequality_is_illegal_2
-    skip
     assert_raises(ArgumentError) do
       triangle = Triangle.new(7, 3, 2)
     end
   end
 
   def test_size_inequality_is_illegal_3
-    skip
     assert_raises(ArgumentError) do
       triangle = Triangle.new(10, 1, 3)
     end
   end
 
   def test_size_inequality_is_illegal_4
-    skip
     assert_raises(ArgumentError) do
       triangle = Triangle.new(1, 1, 2)
     end
