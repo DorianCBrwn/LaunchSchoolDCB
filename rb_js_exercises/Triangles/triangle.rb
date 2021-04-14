bdd6b48cea7e10cb3f0cfc9062fc2d61bf66787b
Dorian Brown <brown.3794@gmail.com>
2021-04-14 13:36:03 -0400

    feat(constructor) complete valid triangle check complete sides_greater_than_zero? and sides_create_tringle?.

--- rb_js_exercises/Triangles/triangle.rb
+++ rb_js_exercises/Triangles/triangle.rb
@@ -7,7 +7,7 @@ OUTPUT:
 Rules:
   Explicit:
         - To be a valid triangle, each side length must be greater than 0.
-        - To be a valid triangle, the sum of the lengths of any two sides must be greater than or equal to the length of the remaining side.
+        - To be a valid triangle, the sum of the lengths of any two sides must be greater than the length of the remaining side.
 
         - An equilateral triangle has three sides of equal length.
         - An isosceles triangle has exactly two sides of the equal length.
@@ -23,17 +23,18 @@ C:
 class Triangle
   attr_accessor :sides
   def initialize(s1,s2,s3)
-    @sides = [s1, s2, s3]
+    validate_triangle(s1,s2,s3)
   end
 
   def kind
     case
-    when self.is_equilateral? then "equilateral"
-    when self.is_isosceles? then "isosceles"
+    when is_equilateral? then "equilateral"
+    when is_isosceles? then "isosceles"
     else "scalene"
     end
-
   end
+
+  private
 
   def is_equilateral?
     sides.all? { |side| @sides.first == side }
@@ -42,5 +43,25 @@ class Triangle
   def is_isosceles?
     sides.tally.values.any? {|side| side == 2}
   end
+
+  def validate_triangle(*initial_sides)
+    if sides_greater_than_zero?(initial_sides)
+      raise ArgumentError
+    elsif sides_create_triangle?(initial_sides)
+      raise ArgumentError
+    end
+    @sides = initial_sides
+  end
+
+  def sides_greater_than_zero?(sides_arr)
+    sides_arr.any? { |side| side <= 0}
+  end
+
+  def sides_create_triangle?(sides_arr)
+    *head, s3 = sides_arr
+    s1, *tail = sides_arr
+    (head.sum <= s3) || (tail.sum <= s1)
+  end
+
 
 end
