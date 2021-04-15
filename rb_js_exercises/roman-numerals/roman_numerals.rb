fb04b2239343c2d8333240ea77d2853923a49ad8
Dorian Brown <brown.3794@gmail.com>
2021-04-15 14:26:46 -0400

    :tada: start of roman_numerals program

---
+++ rb_js_exercises/roman-numerals/roman_numerals.rb
@@ -0,0 +1,25 @@
+=begin
+Problem:
+Rephrase:
+Examples/tests:
+INPUT:
+OUTPUT:
+Rules:
+  Explicit:
+  Implicit:
+D:
+Approach:
+C:
+=end
+
+class RomanNumeral
+
+  def initialize(int)
+    @decimal = int
+    @roman = ''
+  end
+
+  def to_roman
+
+  end
+end
