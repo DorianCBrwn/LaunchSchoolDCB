0a5c659c01176d56dd9208e234e2b9e8fcb83b26
Dorian Brown <brown.3794@gmail.com>
2021-04-15 09:46:24 -0400

    Initial start of point mutations problem

---
+++ rb_js_exercises/point-mutations/point_mutations.rb
@@ -0,0 +1,26 @@
+=begin
+Problem:
+For our program, the details we need to keep in mind are as follows:
+
+We will be given two strands of DNA.
+We need to count the differences between them.
+If one strand is shorter than the other, we only need to check for differences for the length of the shorter strand.
+
+Rephrase:
+Examples/tests:
+INPUT:
+OUTPUT:
+Rules:
+  Explicit:
+         - A constructor that accepts a DNA strand string as argument. The
+          constructor does not raise any errors.
+          - A method that accepts a
+          second DNA strand string as an argument and returns the differences
+          between the two strands — the Hamming distance.
+  Implicit:
+D:
+- We're given the DNA strands as strings.
+- Additionally, we may want to use a collection to help us iterate through each character of the DNA strand.
+Approach:
+C:
+=end
