b1c4a1744fc1788689fa4fd57ac317995ea93a3a
Dorian Brown <brown.3794@gmail.com>
2021-07-10 12:05:38 -0400

    :sparkles: add basic response functionality

---
+++ RB170/simple_http_server/http_server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function server () {
+    while true; do
+        read message path version
+        if [ "$message" == "GET" ]
+        then
+            echo "HTTP/1.1 200 OK"
+        else
+            echo "HTTP/1.1 400 Bad Request"
+        fi
+    done
+}
+
+coproc SERVER_PROCESS { server; }
+
+netcat -lvp 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
