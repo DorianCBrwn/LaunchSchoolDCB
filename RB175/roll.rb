74ccba8502d3bdf8ef77b58ef302f3fa7f2d6da7
Dorian Brown <brown.3794@gmail.com>
2021-07-16 23:56:47 -0400

    style: :art: reformate code with rubocop

--- RB175/roll.rb
+++ RB175/roll.rb
@@ -1,27 +1,26 @@
-require "socket"
+require 'socket'
 
 def parse_request(request_line)
-  http_method, path, q_string_version = request_line.partition("/")
-  q_string, _version = q_string_version.split(" ")
+  http_method, path, q_string_version = request_line.partition('/')
+  q_string, _version = q_string_version.split(' ')
   q_string = q_string[1..-1]
-  q_string = q_string.split("&")
+  q_string = q_string.split('&')
   q_string.each_with_object({}) do |pair, hash|
-    key, value = pair.split("=")
+    key, value = pair.split('=')
     hash[key] = value
   end
 end
 
-server = TCPServer.new("localhost", 3003)
+server = TCPServer.new('localhost', 3003)
 
 loop do
   client = server.accept
   request_line = client.gets
   puts request_line
 
-
   client.puts http_method
   client.puts path
   client.puts q_string
-  client.puts rand(6) + 1
+  client.puts rand(1..6)
   client.close
 end
