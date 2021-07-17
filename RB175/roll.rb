a4933fc09b1932ab55b98ed33cbd314c61b5db49
Dorian Brown <brown.3794@gmail.com>
2021-07-17 02:05:14 -0400

    feat: :sparkles: add rolls response to server

--- RB175/roll.rb
+++ RB175/roll.rb
@@ -1,14 +1,15 @@
 require 'socket'
 
 def parse_request(request_line)
-  http_method, path, q_string_version = request_line.partition('/')
-  q_string, _version = q_string_version.split(' ')
-  q_string = q_string[1..-1]
-  q_string = q_string.split('&')
-  q_string.each_with_object({}) do |pair, hash|
+  http_method, path_and_params, http = request_line.split(' ')
+
+  path, params = path_and_params.split('?')
+
+  params = params.split('&').each_with_object({}) do |pair, hash|
     key, value = pair.split('=')
     hash[key] = value
   end
+  [http_method, path, params]
 end
 
 server = TCPServer.new('localhost', 3003)
@@ -17,10 +18,29 @@ loop do
   client = server.accept
   request_line = client.gets
   puts request_line
+  http_method, path, params = parse_request(request_line)
 
+  client.puts 'HTTP/1.0 200 OK'
+  client.puts 'Content-Type: text/html'
+  client.puts
+  client.puts '<html>'
+  client.puts '<body>'
+  client.puts '<pre>'
   client.puts http_method
   client.puts path
-  client.puts q_string
-  client.puts rand(1..6)
+  client.puts params
+  client.puts '<pre>'
+
+  client.puts '<h1>Rolls</h1>'
+  rolls = params[:rolls].to_i
+  sides = params[:sides].to_i
+  rolls.times do
+    roll = rand(sides) + 1
+    client.puts '<p>', roll, '</p>'
+  end
+
+  client.puts
+  client.puts '<body>'
+  client.puts '<html>'
   client.close
 end
