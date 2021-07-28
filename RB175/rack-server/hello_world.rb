0a130b37d543deedd88f35e35bbdb2781842e15f
Dorian Brown <brown.3794@gmail.com>
2021-07-28 07:34:58 -0400

    feat: :sparkles: Add responses for different request types
    Add basic routing and respones for sucessful response and not found response

--- RB175/rack-server/hello_world.rb
+++ RB175/rack-server/hello_world.rb
@@ -1,5 +1,18 @@
+require_relative 'advice'     # loads advice.rb
 class HelloWorld
-  def call(env) #define method call that takes in parameter env
-    ['200', {'Content-Type' => 'text/plain'}, ['Hello World!']] # return array containing, status code, headers and response body.
+  def call(env)
+    case env['REQUEST_PATH']
+    when '/'
+      ['200', {"Content-Type" => 'text/plain'}, ["<html><body><h2>Hello World!</h2></body></html>"]]
+    when '/advice'
+      piece_of_advice = Advice.new.generate    # random piece of advice
+      ['200', {"Content-Type" => 'text/plain'}, ["<html><body><b><em>#{piece_of_advice}</em></b></body></html>"]]
+    else
+      [
+        '404',
+        {"Content-Type" => 'text/plain', "Content-Length" => '13'},
+        ["<html><body><h4>404 Not Found</h4></body></html>"]
+      ]
+    end
   end
 end
