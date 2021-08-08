ec1dcea19758abcf743498ea814205f13a11df47
Dorian Brown <brown.3794@gmail.com>
2021-08-07 22:57:30 -0400

    feat: :sparkles: add search function
    Search function for book chapters

--- RB175/book_viewer/book_viewer.rb
+++ RB175/book_viewer/book_viewer.rb
@@ -6,6 +6,18 @@ before do
   @contents = File.readlines('data/toc.txt')
 end
 
+helpers do
+  def in_paragraphs(chapter_content)
+    chapter_content.split("\n\n")
+                   .map { |line| "<p>#{line}</p>" }
+                   .join("\n")
+  end
+end
+
+not_found do
+  redirect '/'
+end
+
 get '/' do
   @title = 'The Adventures of Sherlock Holmes'
 
@@ -15,9 +27,40 @@ end
 get '/chapters/:number' do
   number = params[:number].to_i
   chapter_name = @contents[number - 1]
-  @title = "Chapter #{number}: #{chapter_name}"
 
+  redirect '/' unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_name}"
   @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+# Calls the block for each chapter, passing that chapter's number, name, and
+# contents.
+def each_chapter
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+# This method returns an Array of Hashes representing chapters that match the
+# specified query. Each Hash contain values for its :name and :number keys.
+def chapters_matching(query)
+  results = []
+
+  return results if !query || query.empty?
+
+  each_chapter do |number, name, contents|
+    results << { number: number, name: name } if contents.include?(query)
+  end
+
+  results
+end
+
+get '/search' do
+  @results = chapters_matching(params[:query])
+  erb :search
 end
